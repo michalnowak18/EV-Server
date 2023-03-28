fe5143f0b20b6a93f3d1479c5b2c3d9216a70106
Michał Nowak <michalkarol.nowak2001@gmail.com>
2023-03-28 19:21:05 +0200

    Initial commit

---
+++ src/main/java/com/ev/evserver/EvServerApplication.java
@@ -0,0 +1,13 @@
+package com.ev.evserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvServerApplication {
+
+	public static void main( String[] args ) {
+		SpringApplication.run( EvServerApplication.class, args );
+	}
+
+}
---
+++ src/test/java/com/ev/evserver/EvServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ev.evserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
