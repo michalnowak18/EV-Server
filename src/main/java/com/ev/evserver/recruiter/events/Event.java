178997db7de43a60a2c63792a7ea036d60819639
Michał Nowak <michalkarol.nowak2001@gmail.com>
2023-04-02 18:52:50 +0200

    EV-4: Small corrections

--- src/main/java/com/ev/evserver/recruiter/events/Event.java
+++ src/main/java/com/ev/evserver/recruiter/events/Event.java
@@ -6,6 +6,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
+
 import java.sql.Date;
 
 @Getter
--- src/main/java/com/ev/evserver/recruiter/events/EventRepository.java
+++ src/main/java/com/ev/evserver/recruiter/events/EventRepository.java
@@ -1,9 +1,7 @@
 package com.ev.evserver.recruiter.events;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface EventRepository extends JpaRepository<Event, Integer> {
 
 }
