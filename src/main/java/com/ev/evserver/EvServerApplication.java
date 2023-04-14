569fab5de367845025e31f81fce2bd2d89d25980
Michał Nowak <michalkarol.nowak2001@gmail.com>
2023-04-14 20:29:57 +0200

    Added CORS configuration

--- src/main/java/com/ev/evserver/EvServerApplication.java
+++ src/main/java/com/ev/evserver/EvServerApplication.java
@@ -2,6 +2,9 @@ package com.ev.evserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class EvServerApplication {
@@ -10,4 +13,15 @@ public class EvServerApplication {
 		SpringApplication.run( EvServerApplication.class, args );
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST")
+				.allowedHeaders("Content-Type");
+			}
+		};
+	}
 }
--- src/main/java/com/ev/evserver/recruiter/events/EventsController.java
+++ src/main/java/com/ev/evserver/recruiter/events/EventsController.java
@@ -3,14 +3,16 @@ package com.ev.evserver.recruiter.events;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin
 @RestController
-@RequestMapping("/events")
+@RequestMapping(value = "/events",
+consumes = MediaType.APPLICATION_JSON_VALUE,
+produces = MediaType.APPLICATION_JSON_VALUE)
 public class EventsController {
 
 	@Autowired
