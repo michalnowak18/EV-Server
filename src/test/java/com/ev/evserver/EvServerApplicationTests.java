efcebc82ec8e7aa00b9832054845957006e7419b
Michał Nowak <michalkarol.nowak2001@gmail.com>
2023-04-26 18:35:26 +0200

    Added tests for SurveysService and SurveyUtils

--- src/test/java/com/ev/evserver/EvServerApplicationTests.java
+++ src/test/java/com/ev/evserver/EvServerApplicationTests.java
@@ -2,7 +2,9 @@ package com.ev.evserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
+@ContextConfiguration(classes = EvServerApplication.class)
 @SpringBootTest
 class EvServerApplicationTests {
 
