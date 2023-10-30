1e37d175506b4eecab98246037e20dad1270a7b4
julstr1 <strysikjulia@interia.pl>
2023-10-30 22:29:01 +0100

    Extension of the availability editing function

---
+++ src/main/java/com/ev/evserver/recruiter/availability/AvailabilityModificationDto.java
@@ -0,0 +1,12 @@
+package com.ev.evserver.recruiter.availability;
+
+import com.ev.evserver.recruiter.surveys.SurveyDto;
+
+import java.util.List;
+
+public class AvailabilityModificationDto {
+
+    List<AvailabilityDto> availabilityDtoList;
+
+    SurveyDto surveyDto;
+}
